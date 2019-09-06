1a6b678aa33e30703cb73f600344db7ae8d69020
tobiasalanboyd <eboyd@udel.edu>
2019-09-06 18:09:38 -0400

    removed hello world

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
  $(function(){
             //this code runs after page is fully loaded
-            document.getElementById("jumbotron").innerHTML = "Paragraph changed.";
+            
             newFunction();
             $('#nameform').on('submit', function(evt){
                 
-                
+
                 evt.preventDefault();
                 $(".jumbotron").removeClass('d-none');
+                $(".jumbotron").html("a");
 
 
             });
