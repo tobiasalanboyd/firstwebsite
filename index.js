06f6fe39831cd9e47e8b03618f74fb6a810f58e2
tobiasalanboyd <eboyd@udel.edu>
2019-09-06 17:52:38 -0400

    More code

---
+++ index.js
@@ -0,0 +1,16 @@
+ $(function(){
+            //this code runs after page is fully loaded
+            document.getElementById("jumbotron").innerHTML = "Paragraph changed.";
+            newFunction();
+            $('#nameform').on('submit', function(evt){
+                
+                evt.preventDefault();
+                $(".jumbotron").removeClass('d-none');
+
+
+            });
+        
+    });
+    
+    
+
