b9df88fc091f3c330a1ae8787498b0774c1260c2
tobiasalanboyd <eboyd@udel.edu>
2019-09-06 18:20:07 -0400

    dfads

---
+++ index2.js
@@ -0,0 +1,18 @@
+$(function(){
+    //this code runs after page is fully loaded
+    
+    newFunction();
+    $('#nameform').on('submit', function(evt){
+        
+
+        evt.preventDefault();
+        $(".jumbotron").removeClass('d-none');
+        $(".jumbotron").html("a");
+
+
+    });
+
+});
+    
+    
+
